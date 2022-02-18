57aacc9735985ac2125d3869fb5323269bc611f6
priyan <45042030+priyan1995@users.noreply.github.com>
2022-02-18 22:06:22 +0530

    sidemenu UI developed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Sidemenu } from './components/sidemenu/Sidemenu';
 
 function App() {
   return (
     <div className="App">
-     
+
+      <div className='pd-sidemenu-sec'>
+        <Sidemenu />
+      </div>
+
+      <div className='pd-header-sec'>
+
+      </div>
+
     </div>
   );
 }
