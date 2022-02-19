8ec3ea89f87af0fbe80838b0af6b9a75bf90f51b
priyan <45042030+priyan1995@users.noreply.github.com>
2022-02-19 08:36:59 +0530

    sceduled payment added

--- src/components/transactions/Transactions.jsx
+++ src/components/transactions/Transactions.jsx
@@ -3,7 +3,19 @@ import './transactions.css';
 export const Transactions = () => {
     return (
         <>
-            <h5>Transactions</h5>
+            <h5>Scheduled Paymemnts</h5>
+
+            <div className='pd-scd-payment-card'>
+                <p className='pd-date'>  Aug 10, 2021 </p>
+                <p className='pd-name'> M.N.K.Silva </p>
+                <p className='pd-price'> LKR<span>10,000.</span>00 </p>
+            </div>
+
+            <div className='pd-scd-payment-card'>
+                <p className='pd-date'>  Aug 10, 2021 </p>
+                <p className='pd-name'> M.N.K.Silva </p>
+                <p className='pd-price'> LKR<span>10,000.</span>00 </p>
+            </div>
         </>
     )
 }
